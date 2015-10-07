498a702865455d9a8b1366248b24dbeca5778199
Varun Vachhar <varunvachhar@gmail.com>
2015-10-07 19:41:07 -0400

    Circle Animation - Pop animation for circle - Sound effects using web audio - Fixes to deployment

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,31 @@
 var gulp = require('gulp');
 var ghPages = require('gulp-gh-pages');
+var del = require('del');
 
-gulp.task('img', function() {
+gulp.task('clean', function () {
+  return del([
+    './build',
+    './.publish'
+  ]);
+});
+
+gulp.task('img', ['clean'], function() {
   return gulp.src('./img/**/*')
-    .pipe(gulp.dest('./.publish/img'));
+    .pipe(gulp.dest('./build/img'));
 });
 
-gulp.task('dist', function() {
-  return gulp.src('./dist/**/*')
-    .pipe(gulp.dest('./.publish/dist'));
+gulp.task('index', ['img'], function() {
+  return gulp.src('./index.html')
+    .pipe(gulp.dest('./build'));
 });
 
-gulp.task('deploy', ['img', 'dist'], function() {
-  return gulp.src([
-    './.publish/**/*',
-    './index.html'
-  ])
+gulp.task('build', ['index'], function() {
+  return gulp.src('./static/**/*')
+    .pipe(gulp.dest('./build/static'));
+});
+
+gulp.task('deploy', ['build'], function() {
+  return gulp.src('./build/**/*')
     .pipe(ghPages({
       remoteUrl: 'git@github.com:winkerVSbecks/oam-site.git'
     }));
