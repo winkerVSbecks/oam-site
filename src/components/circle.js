88dc69356372a6e6f350f322b21f67dc20227539
Varun Vachhar <varunvachhar@gmail.com>
2015-10-14 11:29:43 -0400

    move styles to their own files and not regenerate animations every render cycle

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import classNames from 'classNames';
-import Radium from 'radium';
 import * as R from 'ramda';
+import Radium from 'radium';
+import {JELLY, SWOOSH_REVERSE} from '../styles/circle-styles';
 import trampolineBounce from '../assets/trampoline-bounce.mp3';
 import quickReverse from '../assets/quick-reverse.mp3';
+
 const bounce = new Audio(trampolineBounce);
 const reverse = new Audio(quickReverse);
 
@@ -19,52 +21,6 @@ export default class Circle extends Component {
 
   render() {
 
-    const jellyKeyframes = Radium.keyframes({
-      '0%': {
-        transform: 'matrix3d(0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '2.7%': {
-        transform: 'matrix3d(0.389, 0, 0, 0, 0, 0.389, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '5.41%': {
-        transform: 'matrix3d(0.916, 0, 0, 0, 0, 0.916, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '8.11%': {
-        transform: 'matrix3d(1.302, 0, 0, 0, 0, 1.302, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '10.71%': {
-        transform: 'matrix3d(1.426, 0, 0, 0, 0, 1.426, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '13.71%': {
-        transform: 'matrix3d(1.306, 0, 0, 0, 0, 1.306, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '16.62%': {
-        transform: 'matrix3d(1.077, 0, 0, 0, 0, 1.077, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '19.62%': {
-        transform: 'matrix3d(0.887, 0, 0, 0, 0, 0.887, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '22.52%': {
-        transform: 'matrix3d(0.826, 0, 0, 0, 0, 0.826, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '28.43%': {
-        transform: 'matrix3d(0.97, 0, 0, 0, 0, 0.97, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '34.33%': {
-        transform: 'matrix3d(1.071, 0, 0, 0, 0, 1.071, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '46.05%': {
-        transform: 'matrix3d(0.971, 0, 0, 0, 0, 0.971, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '57.86%': {
-        transform: 'matrix3d(1.012, 0, 0, 0, 0, 1.012, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '69.57%': {
-        transform: 'matrix3d(0.995, 0, 0, 0, 0, 0.995, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '81.38%': {
-        transform: 'matrix3d(1.002, 0, 0, 0, 0, 1.002, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '93.19%': {
-        transform: 'matrix3d(0.999, 0, 0, 0, 0, 0.999, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }, '100%': {
-        transform: 'matrix3d(1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1);'
-      }
-    }, 'jelly');
-
-    const jellyKeyframesReverse = Radium.keyframes({
-      '0%': {
-        transform: 'scale(1)'
-      }, '100%': {
-        transform: 'scale(0)'
-      }
-    }, 'jellyReverse');
-
     const { fill, x, y, r, visible, mixBlendMode } = this.props;
 
     if (visible) {
@@ -73,7 +29,7 @@ export default class Circle extends Component {
       reverse.play();
     }
 
-    const animation = visible ? jellyKeyframes : jellyKeyframesReverse;
+    const animation = visible ? JELLY : SWOOSH_REVERSE;
     const easing = visible ? 'linear' : 'cubic-bezier(0, 0.79, 0, 1)';
     const time = visible ? '1500ms' : '200ms';
 
